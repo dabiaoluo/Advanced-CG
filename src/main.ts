0de2cc7846f11898ddc2fed0ac5bb06831d427ea
Milky245 <2457761378@qq.com>
2024-01-18 16:45:00 +0900

    test 2 update

--- src/main.ts
+++ src/main.ts
@@ -10,9 +10,9 @@ document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
 
     <div class="header">
       <div class="title">
-        <h1>Nowadays in Tokyo</h1>
-        <p>Learning in Waseda</p>
-      </div>
+      <h1>Favorite Video Games</h1>
+      <p>The world of entertainment, creativity, and social connection with video games, where fun meets skill development for all ages!</p>
+    </div>
       <div class="close-btn">
         <button><img src="/close.svg" alt="" /></button>
       </div>
@@ -21,53 +21,53 @@ document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
     <div class="container">
       <div class="layout-grid">
 
-        <a href="url 1" class="card" target="_blank">
-          <img src="/imgs/game1.jpg" class="img" alt="" />
-          <div class="description">
-            <h3>King of Bots</h3>
-            <p>Ai battle platform supporting PvE and PvP</p>
-          </div>
-        </a>
+      <a href="https://www.counter-strike.net/" class="card" target="_blank">
+      <img src="/imgs/CS2.jpg" class="img" alt="" />
+      <div class="description">
+        <h3>Counter Strike 2</h3>
+        <p>Dive into the future of FPS gaming with Counter-Strike 2: upgraded visuals, fresh maps, and intense action</p>
+      </div>
+    </a>
 
-        <a href="url 2" class="card" target="_blank">
-          <img src="/imgs/game2.jpg" class="img" alt="" />
-          <div class="description">
-            <h3>[Key: Board]</h3>
-            <p>An iOS keyboard for code editing</p>
-          </div>
-        </a>
+    <a href="https://www.ubisoft.com/en-gb/game/rainbow-six/siege" class="card" target="_blank">
+      <img src="/imgs/R6.jpg" class="img" alt="" />
+      <div class="description">
+        <h3>Rainbow Six: Siege</h3>
+        <p>Tactical. Team-based. Thrilling FPS action.</p>
+      </div>
+    </a>
 
-        <a href="url 3" class="card" target="_blank">
-          <img src="/imgs/game3.jpg" class="img" alt="" />
-          <div class="description">
-            <h3>Vue Chemistry</h3>
-            <p>Reactified JavaScript functions for Vue</p>
-          </div>
-        </a>
-        
-        <a href="url 4" class="card" target="_blank">
-          <img src="/imgs/game4.jpg" class="img" alt="" />
-          <div class="description">
-            <h3>Three.js Starting Template</h3>
-            <p>Three.js starting template based on TS and Vite</p>
-          </div>
-        </a>
-        
-        <a href="url 5" class="card" target="_blank">
-          <img src="/imgs/game5.jpg" class="img" alt="" />
-          <div class="description">
-            <h3>Questopia</h3>
-            <p>3D room · Personal website · Online portfolio</p>
-          </div>
-        </a>
+    <a href="https://jp.finalfantasyxvi.com/" class="card" target="_blank">
+      <img src="/imgs/FF16.jpg" class="img" alt="" />
+      <div class="description">
+        <h3>Final Fantasy XVI</h3>
+        <p>Immerse yourself in a visually stunning and epic RPG experience</p>
+      </div>
+    </a>
+    
+    <a href="https://www.elderscrollsonline.com/ja/home" class="card" target="_blank">
+      <img src="/imgs/The Elder Scrolls.jpg" class="img" alt="" />
+      <div class="description">
+        <h3>The Elder Scrolls Online</h3>
+        <p>Immerse yourself in the multiplayer RPG adventure with endless possibilities and epic quests</p>
+      </div>
+    </a>
+    
+    <a href="https://www.ea.com/games/apex-legends?isLocalized=true" class="card" target="_blank">
+      <img src="/imgs/APEX.jpg" class="img" alt="" />
+      <div class="description">
+        <h3>Apex Legend</h3>
+        <p>Action-packed battle royale featuring diverse legends, unique abilities, and intense squad-based competition</p>
+      </div>
+    </a>
 
-        <a href="url 6" class="card" target="_blank">
-          <img src="/imgs/game6.jpg" class="img" alt="" />
-          <div class="description">
-            <h3>Pkg-Desc</h3>
-            <p>List details of all dependencies packages</p>
-          </div>
-        </a>
+    <a href="https://www.dota2.com/home" class="card" target="_blank">
+      <img src="/imgs/DOTA.jpg" class="img" alt="" />
+      <div class="description">
+        <h3>DOTA 2</h3>
+        <p>Dive into intense team battles, master unique heroes, and conquer the online arena in this dynamic multiplayer experience.</p>
+      </div>
+    </a>
         
       </div>
     </div>
@@ -77,8 +77,8 @@ document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
 
     <div class="header">
       <div class="title">
-        <h1>Previous days in China</h1>
-        <p>Life in Sichuan University</p>
+      <h1>Impressive Books</h1>
+      <p>A Good book is a mentor and friend</p>
       </div>
       <div class="prev-close-btn">
         <button><img src="/close.svg" alt="" /></button>
@@ -88,49 +88,48 @@ document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
     <div class="container">
       <div class="layout-grid">
 
-        <a href="url 1" class="card" target="_blank">
-          <img src="/imgs/book1.png" class="img" alt="" />
-          <div class="description">
-            <h3>Pendulum Parking</h3>
-            <p>Maximize the value of idle parking space</p>
-          </div>
-        </a>
-
-        <a href="url 2" class="card" target="_blank">
-          <img
-            src="/imgs/book2.jpg"
-            class="img"
-            alt=""
-          />
-          <div class="description">
-            <h3>Archimason</h3>
-            <p>Online architectral design assistant</p>
-          </div>
-        </a>
-
-        <a href="url 3" class="card" target="_blank">
-          <img
-            src="/imgs/book3.jpg"
-            class="img"
-            alt=""
-          />
-          <div class="description">
-            <h3>Protect Winterfell</h3>
-            <p>Reconstruction of castle wall defense system</p>
-          </div>
-        </a>
-
-        <a href="url 4" class="card" target="_blank">
-          <img src="/imgs/book4.png" class="img" alt="" />
-          <div class="description">
-            <h3>Relevator</h3>
-            <p>Taking elevator in an interesting way</p>
-          </div>
-        </a>
-
+      <a href="https://www.goodreads.com/book/show/34569357-remembrance-of-earth-s-past?from_search=true&from_srp=true&qid=5lDJC1DfjA&rank=2" class="card" target="_blank">
+      <img src="/imgs/book1.jpg" class="img" alt="" />
+      <div class="description">
+        <h3>The Three-Body Problem</h3>
+        <p>"Wildly imaginative, really interesting." —President Barack Obama</p>
       </div>
+    </a>
+
+    <a href="https://www.goodreads.com/book/show/152893.Death_and_the_Penguin" class="card" target="_blank">
+      <img
+        src="/imgs/book2.jpg"
+        class="img"
+        alt=""
+      />
+      <div class="description">
+        <h3>Death_and_the_Penguin</h3>
+        <p>“To every time, its own normality.”</p>
+      </div>
+    </a>
+
+    <a href="https://www.goodreads.com/book/show/61439040-1984" class="card" target="_blank">
+      <img
+        src="/imgs/book3.jpg"
+        class="img"
+        alt=""
+      />
+      <div class="description">
+        <h3>1984</h3>
+        <p>WAR IS PEACE. FREEDOM IS SLAVERY. IGNORANCE IS STRENGTH.</p>
+      </div>
+    </a>
+
+    <a href="https://www.goodreads.com/book/show/61718053-happy-place?from_choice=true" class="card" target="_blank">
+      <img src="/imgs/book4.jpg" class="img" alt="" />
+      <div class="description">
+        <h3>Happy Place</h3>
+        <p>They broke up six months ago. And still haven’t told their best friends.</p>
+      </div>
+    </a>
     </div>
   </div>
+</div>
 `
 
 export { experience }
